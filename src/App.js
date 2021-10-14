00e2399abaf9901b1dc28ce99c8757867bf9cd95
Bionca Bond <bioncab@zillowgroup.com>
2021-10-14 11:28:53 -0700

    full_lab_07

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import axios from 'axios';
-import Map from './Components/map.js';
-import Weather from './Components/weather.js';
-import ErrorMessage from './Components/ErrorMessage.js';
+import Map from './Components/Map';
+import Weather from './Components/Weather';
+import ErrorMessage from './Components/ErrorMessage';
 
 class App extends React.Component {
-  constructor(props){
-    super(props); 
-    this.state={
-      cityName: '',  
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityName: '',
       locationObj: '',
-      mapIMG: '',
       errorCode: '',
       errorAlert: false,
       mapImg: '',
@@ -18,86 +17,74 @@ class App extends React.Component {
       showWeather: false
 
     }
-  }  
+  }
 
   //functions
+  onErrorClose = () => {
+    this.setState({ errorAlert: false })
+  }
+  getLocation = async (event) => {
+    event.preventDefault();
+    console.log(`button pushed:`);
+    let apiKey = 'pk.b5c86569d4017c823b239e4acadfb112'
+    let URL = `https://us1.locationiq.com/v1/search.php?key=${apiKey}&q=${this.state.cityName}&format=json`;
+    console.log(URL);
 
 
-onErrorClose = () => {
-  this.setState({errorAlert : false})
-}
-
-  getLocation = async () => {
-      console.log(`button pushed:`);
-      let apiKey = 'pk.b5c86569d4017c823b239e4acadfb112'
-      let URL = `https://us1.locationiq.com/v1/search.php?key=${apiKey}&q=${this.state.cityName}&format=json`; 
-      
-
-      console.log(URL);
-
-      try{
-      let locData = await axios.get(URL); 
+    try {
+      let locData = await axios.get(URL);
       console.log(locData.data);
-        this.setState({locationObj: locData.data[0]});
+      this.setState({ locationObj: locData.data[0] });
 
-                
-        let mapURL = `https://maps.locationiq.com/v3/staticmap?key=${apiKey}&center=${this.state.locationObj.lat},${this.state.locationObj.lon}&zoom=12`;
-        this.setState({mapImg: mapURL});
-        console.log(this.state.mapImg); 
+      let mapURL = `https://maps.locationiq.com/v3/staticmap?key=${apiKey}&center=${this.state.locationObj.lat},${this.state.locationObj.lon}&zoom=12`;
+      this.setState({ mapImg: mapURL });
+      console.log(this.state.mapImg);
 
-        let allWeatherArray = await axios.get(`http://localhost:3001/weather?searchQuery=${this.state.cityName}`);
-        this.setState({weatherData : allWeatherArray, showWeather: true});
-        console.log(this.state.weatherData);
+      let allWeatherArray = await axios.get(`http://localhost:3001/weather?searchQuery=${this.state.cityName}`);
+      this.setState({ weatherData: allWeatherArray.data, showWeather: true });
+      console.log(this.state.weatherData);
 
+    }
+     
+      catch(error) {
+        console.log('there as an error:', error)
+        this.setState({ errorCode: error.message })
+        this.setState({ errorAlert: true })
+    };
+  }
+ 
+
+render(){
+  return (
+    <>
+      <h1>Where are we going?</h1>
+
+      <input onChange={(event) => this.setState({ cityName: event.target.value })}></input>
+      <button onClick={this.getLocation}>Let's GO!</button>
+      <h2> City Found:{this.state.locationObj.display_name}</h2>
+      <h4> Latitude:{this.state.locationObj.lat}</h4>
+      <h4> Longitude:{this.state.locationObj.lon}</h4>
+
+
+      {this.state.showWeather && this.state.weatherData.map((el) =>
+        <Weather date={el.date} description={el.description} />)
       }
 
-
-        let mapURL= `https://maps.locationiq.com/v3/staticmap?key=$pk.b5c86569d4017c823b239e4acadfb112&center=${this.props.locationObj.lat},${this.props.locationObj.lon}&zoom=9`;
-        this.setState({
-          mapIMG: mapURL
-        });
+      {this.state.locationObj && 
+        <Map
+          mapImg={this.state.mapImg}
+          locationObj={this.state.locationObj}
+        />
       }
-      catch (error) {
-        console.log('there as an error:' , error)
-        this.setState({errorCode:error.message})
-        this.setState({errorAlert:true})
-      }
-    } 
-  
-  render(){
-    return (
-      <>
-    <h1>Where are we going?</h1> 
-
-    {/* <h3>{this.state.cityName}</h3> */}
-    <input onChange={(event) => this.setState({cityName: event.target.value})}>
-    </input>
-    <button onClick ={this.getLocation}>Let's GO!</button>
-    <h2> City Found:{this.state.locationObj.display_name}</h2>
-    <h4> Latitude:{this.state.locationObj.lat}</h4>
-    <h4> Longitude:{this.state.locationObj.lon}</h4>
-    <Map mapImg={this.state.mapImg}/>    
-    
-    {this.state.showWeather && this.state.weatherData.map((el) =>
-          <Weather date={el.date} description={el.description} />)
-        }
-
-    <h2> City Found:{this.state.locationObj.display_name}</h2>
-   
-   { this.state.locationObj &&
-     <Map
-      mapIMG={this.state.locationObj}
-      locationObj={this.state.locationObj}
-    />
-   }
-<ErrorMessage 
- errorCode={this.state.errorCode} 
- errorAlert={this.state.errorAlert}  
- onErrorClose={this.onErrorClose}
-/>
+      <ErrorMessage
+        errorCode={this.state.errorCode}
+        errorAlert={this.state.errorAlert}
+        onErrorClose={this.onErrorClose}
+      />
     </>
   );
 } 
 }
+
 
 export default App;
--- src/Components/weather.js
+++ src/Components/weather.js
@@ -1,20 +1,15 @@
-
 import React from 'react';
 
+
 class Weather extends React.Component {
-  
     render() {
-      
       return (
         <>
           <div className="m-2">
            <p>{this.props.date}: {this.props.description}</p>
           </div>
-  
-        </>
-  
+          </>
       )
     }
   }
-  
   export default Weather;
